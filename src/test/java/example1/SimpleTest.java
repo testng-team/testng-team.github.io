0f28f2b7eabc265bd684ea0ad503c886febcf8f8
Krishnan Mahadevan <krishnan.mahadevan1978@gmail.com>
2024-01-09 17:20:46 +0530

    Revamped documentation using asciidoctor

---
+++ src/test/java/example1/SimpleTest.java
@@ -0,0 +1,22 @@
+package example1;
+
+import org.testng.annotations.*;
+
+public class SimpleTest {
+
+    @BeforeClass
+    public void setUp() {
+        // code that will be invoked when this test is instantiated
+    }
+
+    @Test(groups = {"fast"})
+    public void aFastTest() {
+        System.out.println("Fast test");
+    }
+
+    @Test(groups = {"slow"})
+    public void aSlowTest() {
+        System.out.println("Slow test");
+    }
+
+}
